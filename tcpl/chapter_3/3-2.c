da1125e7be1c66204c943cb829ad0031b65db045
chen-chao <wenbushi@gmail.com>
2017-10-07 18:14:46 +0800

    add 3-2.c description

--- tcpl/chapter_3/3-2.c
+++ tcpl/chapter_3/3-2.c
@@ -1,3 +1,11 @@
+/* The C Programming Language, 2nd Edition, by Kernighan and Ritchie,
+ * Exercise 3.02 on page 60. Write a function escape(s,t) that
+ * converts characters like newline and tab into visible escape
+ * sequences like \n and \t as it copies the string t to s . Use a
+ * switch . Write a function for the other direction as well,
+ * converting escape sequences into the real characters. */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
