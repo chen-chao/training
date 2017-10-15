bd6357703e934a8b18c219eb1cd81a1f2e75f167
chen-chao <wenbushi@gmail.com>
2017-10-15 10:13:11 +0800

    tcpl 4-4: more stack operations

--- tcpl/chapter_4/calculator/stack.c
+++ tcpl/chapter_4/calculator/stack.c
@@ -22,3 +22,38 @@ double pop(void){
                 return 0.0;
         }
 }
+
+void print(void){
+        if (sp > 0){
+                printf("%.8g", val[sp-1]);
+        } else {
+                printf("error: stack empty\n");
+        }
+}
+
+double copy(void){
+        if (sp > 0){
+                return val[sp-1];
+        }
+        else{
+                printf("error: stack empty\n");
+                return 0.0;
+        }
+}
+
+void change(void){
+        double tmp;
+        if (sp > 1){
+                tmp = val[sp-1];
+                val[sp-1] = val[sp-2];
+                val[sp-2] = tmp;
+        } else if (sp < 1) {
+                printf("error: stack empty\n");
+        }
+}
+
+void clear(void){
+        sp = 0;
+}
+
+
