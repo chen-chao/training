4e9ceca06abea38ce90e0d48223fa7230b72fce6
chen-chao <wenbushi@gmail.com>
2017-11-08 11:10:59 +0800

    add numeric option -n for sorttext

--- tcpl/chapter_5/sorttext/sorttext.c
+++ tcpl/chapter_5/sorttext/sorttext.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 #define MAXLINES 5000
 
 char *lineptr[MAXLINES];
 
 int readlines(char *lineptr[], int nlines);
 void writelines(char *lineptr[], int nlines);
-void qsort(char *lineptr[], int left, int right);
+void quicksort(void *v[], int left, int right, int (*comp)(void *, void *));
+int numcmp(char *s1, char *s2);
 
-int main(){
+int main(int argc, char *argv[]){
     int nlines;
+    int numeric = 0;
 
+    if (argc > 1 && strcmp(argv[1], "-n")==0)
+        numeric = 1;
     if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
-        qsort(lineptr, 0, nlines-1);
+        quicksort(lineptr, 0, nlines-1, (int (*)(void*, void*))(numeric ? numcmp : strcmp));
         writelines(lineptr, nlines);
         return 0;
     } else {
