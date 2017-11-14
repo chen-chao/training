4d64979095d355c5c8c40e72cf6f58b51f4cc568
chen-chao <wenbushi@gmail.com>
2017-11-14 21:59:58 +0800

    add ascend argument to quicksort for reverse option

--- tcpl/chapter_5/sorttext/quicksort.c
+++ tcpl/chapter_5/sorttext/quicksort.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
-void  quicksort(void *v[], int left, int right, int (*comp)(void *, void *)){
+void  quicksort(void *v[], int left, int right, int (*comp)(void *, void *), int ascend){
     int i, last;
     void swap(void *v[], int, int);
-
+    int sign = (ascend==1) ? 1 : -1;
+    
     if (left >= right)
         return;
     swap(v, left, (left+right)/2);
     last = left;
     for (i = left+1; i <= right; i++)
-        if ((*comp)(v[i], v[left]) < 0)
+        if (sign*(*comp)(v[i], v[left]) < 0)
             swap(v, ++last, i);
     swap(v, left, last);
-    quicksort(v, left, last-1, comp);
-    quicksort(v, last+1, right, comp);
+    quicksort(v, left, last-1, comp, ascend);
+    quicksort(v, last+1, right, comp, ascend);
 }
 
 #include <stdlib.h>
@@ -25,7 +26,7 @@ void swap(void *v[], int i, int j){
     v[j] = temp;
 }
 
-int numcmp(char *s1, char *s2){
+int numcmp(const char *s1, const char *s2){
         double v1, v2;
         v1 = atof(s1);
         v2 = atof(s2);
@@ -37,14 +38,3 @@ int numcmp(char *s1, char *s2){
                 return 0;
 }
 
-int reverse_comp(int (*comp)(void *, void *), char *s1, char *s2){
-        return -1*comp(s1, s2);
-}
-
-int reverse_numcmp(char *s1, char *s2){
-        return reverse_comp((int (*) (void *, void *))numcmp, s1, s2);
-}
-
-int reverse_strcmp(char *s1, char *s2){
-        return reverse_comp((int (*) (void *, void *))strcmp, s1, s2);
-}
--- tcpl/chapter_5/sorttext/sorttext.c
+++ tcpl/chapter_5/sorttext/sorttext.c
@@ -6,15 +6,13 @@ char *lineptr[MAXLINES];
 
 int readlines(char *lineptr[], int nlines);
 void writelines(char *lineptr[], int nlines);
-void quicksort(void *v[], int left, int right, int (*comp)(void *, void *));
-int numcmp(char *s1, char *s2);
-int reverse_numcmp(char *s1, char *s2);
-int reverse_strcmp(char *s1, char *s2);
+void quicksort(void *v[], int left, int right, int (*comp)(void *, void *), int ascend);
+int numcmp(const char *s1, const char *s2);
 
 int main(int argc, char *argv[]){
     int nlines;
     int numeric = 0;
-    int reverse = 0;
+    int ascend = 1;
     int c;
     while(--argc > 0 && (*++argv)[0] == '-'){
             while ((c = *++argv[0])){
@@ -23,7 +21,7 @@ int main(int argc, char *argv[]){
                             numeric = 1;
                             break;
                     case 'r':
-                            reverse = 1;
+                            ascend = 0;
                             break;
                     default:
                             argc = -1;
@@ -37,9 +35,9 @@ int main(int argc, char *argv[]){
     }
 
     int (*comp) (void *, void *);
-    comp = numeric? (reverse? reverse_numcmp : numcmp) : (reverse? reverse_strcmp : strcmp);
+    comp = (int (*)(void *, void *))(numeric ? numcmp : strcmp);
     if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
-            quicksort((void **)lineptr, 0, nlines-1, comp);
+            quicksort((void **)lineptr, 0, nlines-1, comp, ascend);
             writelines(lineptr, nlines);
             return 0;
     } else {
