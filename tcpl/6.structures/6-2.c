f99dddb4d202ac62f66c51e533af2ed1abb0c4b2
chen-chao <wenbushi@gmail.com>
2017-12-20 12:29:09 +0800

    tcpl 6-2: improve group words started with same letters

--- tcpl/6.structures/6-2.c
+++ tcpl/6.structures/6-2.c
@@ -1,3 +1,10 @@
+/* The C Programming Language, 2nd Edition, by Kernighan and Ritchie,
+ * Exercise 6.02 on page 143. Write a program that reads a C program
+ * and prints in alphabetical order each group of variable names that
+ * are identical in the first 6 characters but different somewhere
+ * thereafter. Don't count words within strings and comments. Make 6 a
+ * parameter that can be set from the command line. */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -45,6 +52,9 @@ void treeprint(struct tnode *p, char *sep){
         treeprint(p->right, sep);
 }
 
+/* here can use static variable to indicate whether the previous word
+ * is printed, see group_with_previous, from
+ * http://clc-wiki.net/wiki/K%26R2_solutions:Chapter_6:Exercise_2 */
 struct tnode *group(struct tnode *p, char *out, int n){
     struct tnode *node;
     if (p == NULL)
@@ -79,6 +89,39 @@ struct tnode *group(struct tnode *p, char *out, int n){
     return p;
 }
 
+void group_with_previous(struct tnode *p, int n){
+    static struct tnode *previous;
+    static unsigned print_previous = 1;
+
+    /* This must be before the statements in (p != NULL), to print
+     * the last word with print_previous */
+    if (previous != NULL && print_previous == 1){
+        printf("%s", previous->word);
+        print_previous = 0;
+    }
+        
+    if (p != NULL){
+        group_with_previous(p->left, n);
+        if (n <= 0) {
+            printf("%s\n", p->word);
+        } else {
+            if (previous != NULL){
+                if (strneq(p->word, previous->word, n) == 1){
+                    printf(" %s", p->word);
+                    print_previous = 0;
+                } else {
+                    printf("\n");
+                    print_previous = 1;
+                }
+            } else {
+                print_previous = 1;
+            }
+            previous = p;
+        }
+        group_with_previous(p->right, n);
+    }
+}
+
 int main(int argc, char *argv[]){
     int n;
     n = (argc == 2) ? atoi(argv[1]) : 6; 
@@ -93,5 +136,7 @@ int main(int argc, char *argv[]){
     }
     group(p, out, n);
     printf("%s\n", out);
+    printf("=====================================\n");
+    group_with_previous(p, n);
     return 0;
 }
