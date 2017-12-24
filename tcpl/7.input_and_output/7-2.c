802422dba6943ec6dcc32e082c58d0b31b4aa991
chen-chao <wenbushi@gmail.com>
2017-12-24 17:28:35 +0800

    tcpl 7-2: print chars in a sensible way

---
+++ tcpl/7.input_and_output/7-2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define MAX_ChAR_LINE 79
+
+void sensible_printf(){
+    int count, c;
+
+    for (count = 0; (c = getchar()) != EOF; count++){
+        if (count > MAX_ChAR_LINE && c != '\n'){
+            putchar('\n');
+            count = 0;
+        } else if (c == 10){
+            count = 0;
+            putchar(c);
+        } else if (c < 32 || c > 126){
+            printf("%x", c);
+        } else {
+            putchar(c);
+        }
+    }
+}
+
+int main(){
+    sensible_printf();
+    return 0;
+}
