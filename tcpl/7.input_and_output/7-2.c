50b2819bac406020f67c3d7eeae2692dc3c634b9
chen-chao <wenbushi@gmail.com>
2017-12-28 16:27:23 +0800

    FIX: tcpl 7-2, missing char when splitting the line

--- tcpl/7.input_and_output/7-2.c
+++ tcpl/7.input_and_output/7-2.c
@@ -6,10 +6,11 @@ void sensible_printf(){
     int count, c;
 
     for (count = 0; (c = getchar()) != EOF; count++){
-        if (count > MAX_ChAR_LINE && c != '\n'){
+        if (count == MAX_ChAR_LINE && c != '\n'){
             putchar('\n');
             count = 0;
-        } else if (c == 10){
+        } 
+        if (c == 10){
             count = 0;
             putchar(c);
         } else if (c < 32 || c > 126){
