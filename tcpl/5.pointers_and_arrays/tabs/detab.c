fd6d4b5e6b25bf4d3683a8767396b41b56b6fd42
chen-chao <wenbushi@gmail.com>
2018-01-11 19:42:14 +0800

    tcpl 5-12: add -m +n support for entab and detab

--- tcpl/5.pointers_and_arrays/tabs/detab.c
+++ tcpl/5.pointers_and_arrays/tabs/detab.c
@@ -2,38 +2,60 @@
 #include <stdlib.h>
 
 
-int detab(int *tabpos);
+#define MAXLEN 30
+
+int detab(int *tabpos, int maxlen);
 
 int main(int argc, char *argv[]){
-        int tabpos[] = {8, 16, 24, 32, 40, 48, 56, 64, 72,
-                        80, 88, 96, 104, 112, 120, 128, 136};
+    int tabpos[MAXLEN], start, width = 0, i = 0;
+    if (argc == 1) {
+        start = 8;
+        width = 8;
+                      }
 
-        if (argc != 1){
-                for (int i = 0; i < argc-1; i++)
-                        tabpos[i] = atoi(argv[i+1]);
+    while (--argc > 0) {
+        switch ((*++argv)[0]) {
+        case '-':
+            start = atoi(++argv[0]);
+            break;
+        case '+':
+            width = atoi(++argv[0]);
+            break;
+        default:
+            tabpos[i++] = atoi(*argv);
+            break;
         }
+    }
 
-        int status;
-        while (1) {
-                status = detab(tabpos);
-                if (status != 0)
-                        return -1;
-        }
+    if (i == 0) {
+        start = start == 0 ? 8 : start;
+        width = width == 0 ? 8 : width;
+    }
+    
+    if (width != 0)
+        for (i = 0; i < MAXLEN; i++)
+            tabpos[i] = start + width*i;
+
+    int status;
+    while ((status = detab(tabpos, MAXLEN)) == 0)
+        ;
+    return status;
 }
 
-int detab(int *tabpos){
-        int c, tabind = 0, col = 0;
-        while ((c = getchar()) != EOF){
-                if (c == '\t') {
-                        for (; tabpos[tabind]<col; tabind++)
-                                ;
-                        /* should tab actually occupy a position? */
-                        for (; col<tabpos[tabind]; col++)
-                                putchar(' ');
-                } else {
-                        putchar(c);
-                        col++;
-                }
+int detab(int *tabpos, int len){
+    int c, ind = 0, col = 0;
+    while ((c = getchar()) != EOF){
+        col++;
+        if (c == '\t') {
+            for (; tabpos[ind] < col && ind < len; ind++)
+                ;
+            if (ind == len)
+                return -2;
+            for (putchar(' '); col < tabpos[ind]; col++)
+                putchar(' ');
+        } else {
+            putchar(c);
         }
-        return (c == EOF) ? -1 : 0;
+    }
+    return (c == EOF) ? -1 : 0;
 }
--- tcpl/5.pointers_and_arrays/tabs/entab.c
+++ tcpl/5.pointers_and_arrays/tabs/entab.c
@@ -2,47 +2,73 @@
 #include <stdlib.h>
 
 
-int entab(int *tabpos);
+#define MAXLEN 30
+
+int entab(int *tabpos, int len);
 
 int main(int argc, char *argv[]){
-        int tabpos[] = {8, 16, 24, 32, 40, 48, 56, 64, 72,
-                        80, 88, 96, 104, 112, 120, 128, 136};
-
-        if (argc != 1){
-                for (int i = 0; i < argc-1; i++)
-                        tabpos[i] = atoi(argv[i+1]);
+    int tabpos[MAXLEN], start, width = 0, i = 0;
+    if (argc == 1) {
+        start = 8;
+        width = 8;
+    }
+        
+    while (--argc > 0) {
+        switch ((*++argv)[0]) {
+        case '-':
+            start = atoi(++argv[0]);
+            break;
+        case '+':
+            width = atoi(++argv[0]);
+            break;
+        default:
+            tabpos[i++] = atoi(*argv);
+            break;
         }
+    }
 
-        int status;
-        while (1) {
-                status = entab(tabpos);
-                if (status != 0)
-                        return -1;
-        }
+    if (i == 0) {
+        start = start == 0 ? 8 : start;
+        width = width == 0 ? 8 : width;
+    }
+    
+    if (width != 0)
+        for (i = 0; i < MAXLEN; i++)
+            tabpos[i] = start + width*i;
+
+    int status;
+    while ((status = entab(tabpos, MAXLEN)) == 0)
+        ;
+    return status;
 }
 
-int entab(int *tabpos){
-        int lastc, c, ind = 0, col = 0, spacenum = 0;
-        while ((c = getchar()) != EOF){
-                if (c == ' ') {
-                        for (; tabpos[ind]<col; ind++)
-                                ;
-                        if (col == tabpos[ind]){
-                                putchar('\t');
-                                /* putchar('t'); */
-                                spacenum = 0;
-                        }
-                        spacenum++;
-                } else {
-                        if (lastc == ' ')
-                                for (int i=0; i<spacenum; i++)
-                                        putchar(' ');
-                        spacenum = 0;
-                        putchar(c);
-                }
-                lastc = c;
-                col++;
-        }
-        return (c == EOF) ? -1 : 0;
-}
+int entab(int *tabpos, int len){
+    int c, ind = 0, col = 0, spacenum = 0;
+    while ((c = getchar()) != EOF && ind < len){
+        /* assume column starts from 1 */
+        col++;
+        if (tabpos[ind] < col)
+            ind++;
 
+        if (ind == len)
+            return -2;
+        
+        if ( c != ' ') {
+            if (spacenum != 0)
+                for (; spacenum > 0; spacenum--)
+                    putchar(' ');
+            putchar(c);
+        } else {
+            if (col == tabpos[ind]){
+                putchar('\t');
+#ifdef DEBUG
+                putchar('\\');
+                putchar('t');
+#endif
+                spacenum = 0;
+            } else
+                spacenum++;
+        }
+    }
+    return (c == EOF) ? -1 : 0;
+}
