6bdf1acbb3155ead060e946e14c7f7049f4c19bc
chen-chao <cchao.im@gmail.com>
2018-02-20 17:08:03 +0800

    Percolation: use 2 wquuf to implement isFull(avoid non-full element connected to the virtual points)

--- Percolation.java
+++ Percolation.java
@@ -1,76 +1,96 @@
+// import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     private boolean[][] structure;
     private int openSiteNum = 0;
-    private int size;
-    private WeightedQuickUnionUF uf;
+    private final int size;
+    private final WeightedQuickUnionUF uf, ufWithoutBottom;
 
     public Percolation(int n) {
         // create n-by-n grid, with all sites blocked
-        if (n <= 0) {throw new java.lang.IllegalArgumentException();}
+        if (n <= 0) { throw new java.lang.IllegalArgumentException(); }
         size = n;
-        structure = new boolean[size+2][size+2];
-        for (int i = 0; i < size+2; i++) {
-            for (int j = 0; j < size+2; j++) {
+        structure = new boolean[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 structure[i][j] = false;
             }
         }
 
         // we don't include those boundary grids
-        int last = size*size+1;
+        int last = size * size + 1;
         uf = new WeightedQuickUnionUF(last+1);
+        ufWithoutBottom = new WeightedQuickUnionUF(last);
         for (int i = 1; i <= size; i++) {
             uf.union(0, i);
+            ufWithoutBottom.union(0, i);
             uf.union(last, last-i);
         }
     }
     private void isValid(int row, int col) {
-        if (row < 1 || col < 1 || row > size || col > size ) {throw new java.lang.IllegalArgumentException();}
+        if (row < 1 || col < 1 || row > size || col > size) {
+            throw new java.lang.IllegalArgumentException();
+        }
     }
     private int coord(int row, int col) {
         // isValid(row, col); isValid hasn't been called because we ensure the validness of row and col
         return (row-1)*size+col;
     }
 
+    private boolean innerOpen(int row, int col) {
+        return (row >= 1 && col >= 1 && row <= size && col <= size && structure[row-1][col-1]);
+    }
+
     public void open(int row, int col) {
         // open site (row, col) if it is not open already
-        isValid(row, col);
         if (isOpen(row, col)) { return; }
-        structure[row][col] = true;
+        structure[row-1][col-1] = true;
         openSiteNum++;
         int arr = coord(row, col);
-        // we can't use isOpen here because of the isValid check
-        if (structure[row-1][col]) { uf.union(arr, coord(row-1, col)); }
-        if (structure[row+1][col]) { uf.union(arr, coord(row+1, col)); }
-        if (structure[row][col-1]) { uf.union(arr, coord(row, col-1)); }
-        if (structure[row][col+1]) { uf.union(arr, coord(row, col+1)); }
+        if (innerOpen(row-1, col)) { 
+            uf.union(arr, coord(row-1, col)); 
+            ufWithoutBottom.union(arr, coord(row-1, col)); 
+        }
+        if (innerOpen(row+1, col)) { 
+            uf.union(arr, coord(row+1, col)); 
+            ufWithoutBottom.union(arr, coord(row+1, col)); 
+        }
+        if (innerOpen(row, col-1)) { 
+            uf.union(arr, coord(row, col-1)); 
+            ufWithoutBottom.union(arr, coord(row, col-1)); 
+        }
+        if (innerOpen(row, col+1)) { 
+            uf.union(arr, coord(row, col+1)); 
+            ufWithoutBottom.union(arr, coord(row, col+1)); 
+        }
     }
 
     public boolean isOpen(int row, int col) {
         // is site (row, col) open?
         isValid(row, col);
-        return structure[row][col];
+        return structure[row-1][col-1];
     }
+
     public boolean isFull(int row, int col) {
         // is site (row, col) full?
-        isValid(row, col);
-        return !structure[row][col];
+        return isOpen(row, col) && ufWithoutBottom.connected(0, coord(row, col));
     }
+
     public int numberOfOpenSites() {
         // number of open sites
         return openSiteNum;
     }       
     public boolean percolates() {
         // does the system percolate?
-        return uf.connected(0, size*size+1);
+        return size == 1 ? isOpen(1, 1) : uf.connected(0, size*size+1);
     }             
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // test client (optional)
-        int N = StdIn.readInt();
-        Percolation perco = new Percolation(N);
+        int n = StdIn.readInt();
+        Percolation perco = new Percolation(n);
         while (!StdIn.isEmpty()) {
             int p = StdIn.readInt();
             int q = StdIn.readInt();
@@ -78,5 +98,7 @@ public class Percolation {
         }
         if (perco.percolates()) { System.out.println("Percolates!"); }
         else { System.out.println("Not percolates!"); }
+        // StdOut.print(perco.isFull(9, 1));
+        // perco.print();
     }
 }
--- PercolationStats.java
+++ PercolationStats.java
@@ -1,56 +1,55 @@
-import java.lang.Math;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.StdOut;
 
 public class PercolationStats {
+    private static final double CONFIDENCE_95 = 1.96;
     private final double[] result;
     private final int trials;
+    private final double meanv, stdv;
+
     public PercolationStats(int n, int trials) {
         if (n <= 0 || trials <= 0) { throw new java.lang.IllegalArgumentException(); }
         this.trials = trials;
         result = new double[trials];
-        for (int i = 0; i < trials; i++) {
-            result[i] = simulation(n);
+        for (int i = 0; i < trials; i++) { result[i] = simulation(n); }
+        meanv = StdStats.mean(result);
+        stdv = StdStats.stddev(result);
+    }
+
+    private double simulation(int n) {
+        Percolation perco = new Percolation(n);
+        int size, rand, row, col; 
+        size = n*n;
+        while (!perco.percolates()) {
+            do {
+                rand = StdRandom.uniform(size);
+                row = rand / n + 1;
+                col = rand % n + 1;
+            } while (perco.isOpen(row, col));
+            perco.open(row, col);
         }
+        // StdOut.println(perco.numberOfOpenSites());
+        return perco.numberOfOpenSites()*1.0/(n*n);
     }
 
-	private double simulation(int n) {
-		Percolation perco = new Percolation(n);
-		int size, rand, row, col; 
-		size = n*n;
-		while (!perco.percolates()) {
-			do {
-				rand = StdRandom.uniform(size);
-				row = rand / n + 1;
-				col = rand % n + 1;
-			} while (perco.isOpen(row, col));
-			perco.open(row, col);
-		}
-		// StdOut.println(perco.numberOfOpenSites());
-		return perco.numberOfOpenSites()*1.0/(n*n);
-	}
+    public double mean() { return meanv; }
 
-    public double mean() {
-        return StdStats.mean(result);
-    }
-
-    public double stddev() {
-        return StdStats.stddev(result);
-    }
+    public double stddev() { return stdv; }
 
     public double confidenceLo() {
-        return mean()-1.96*stddev()/Math.sqrt(trials);
+        return meanv - CONFIDENCE_95 * stdv / Math.sqrt(trials);
     }
 
     public double confidenceHi() {
-        return mean()+1.96*stddev()/Math.sqrt(trials);
+        return meanv + CONFIDENCE_95 * stdv / Math.sqrt(trials);
     }
+
     public static void main(String[] args) {
         int n, trials;
         if (args.length < 2) {
-            n = 50; 
-            trials = 50;
+            n = 200; 
+            trials = 100;
         } else {
             n = Integer.parseInt(args[0]);
             trials = Integer.parseInt(args[1]);
